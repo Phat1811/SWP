f4d395bb7a6a8e87660b0145d59d872eee80e368
minhthuan191 <minhthuan1912001@gmail.com>
2022-01-14 10:42:13 +0700

    Code first DB

---
+++ Models/Order.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MedicalStore.Models
+{
+
+        public enum OrderStatus
+        {
+            ACTIVE = 1,
+            INACTIVE = 0
+        }
+
+
+        [Table("tblOrder")]
+        public class Order
+        {
+
+
+            [Required]
+            [StringLength(50)]
+            [Key]
+            public string OrderId { set; get; }
+
+            [Required]
+            public OrderStatus Status { get; set; }
+
+            [Required]
+            public float Total { get; set; }
+
+            [Required]
+            [StringLength(50)]
+            public string CreateDate { set; get; }
+
+            [Required]
+            [StringLength(50)]
+            [ForeignKey("tblUser")]
+            public string CustomerId { set; get; }
+
+            public virtual User Customer { set; get; }
+        }
+    
+}
